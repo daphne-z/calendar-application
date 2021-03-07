f807f5143c4ffc0029be8b7e1e3085273bbd2f40
Daphne Zaborowski <dezaborowski@gmail.com>
2021-03-07 15:50:44 -0600

    commit 4 - started coding the local storage portion of the app

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // save reference to important DOM elements
 var dateDisplayEl = $('#currentDay');
+var taskText = document.getElementByTagName("textarea").value;
 
 // handle displaying the date
 function displayDate() {
@@ -8,3 +9,11 @@ function displayDate() {
 };
 
 displayDate();
+
+document.getElementsByClassName('saveBtn').onclick, function saveText() {
+  localStorage.setItem("tastText", taskText);
+};
+
+//Save responses to local storage
+//Change color of rows based on whether they are in the past, present or future
+//Add an icon to the save button
