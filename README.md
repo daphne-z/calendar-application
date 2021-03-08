8c7bab38d040df0178b5d5d703b50c9507339b48
Daphne Zaborowski <dezaborowski@gmail.com>
2021-03-08 15:32:56 -0600

    adding some screenshots

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # calendar-application
 
-Shout out to the following resources:
+This calendar app is was designed to help the user keep track of their to-do items for different times of the work day. 
+
+![screenshot](/images/screenshot-1.png?raw=true)
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 var dateDisplayEl = $('#currentDay');
 var taskTextEl = $("textarea");
 
+
 // handle displaying the date
 function displayDate() {
   var rightNow = moment().format("dddd, MMMM Do YYYY");
@@ -12,14 +13,23 @@ function displayDate() {
 
 displayDate();
 
-//Save responses to local storage
-$('.saveBtn').on('click', function () {
-  var task = $(this).siblings() ;
-        localStorage.setItem("task", task.value) ;
-        taskDisplayEl.text(task);
-});
+// //Save responses to local storage
+// $('.saveBtn').on('click', function () {
+//   var task = $(this).siblings('.description').val();
+//   console.log("task", task);
+//   localStorage.setItem("task", task) ;
+//   taskTextEl.text(task);
+// });
 
+//pseudo code:
 //Change color of rows based on whether they are in the past, present or future
-//Add an icon to the save button
+//assign each time block a start time and an end time
+//write a function that compares the currentTime to the start/end time of each time block
+//if the current time is after the time block, then the textarea background is gray
+//if the current time is during the time block, then the textarea background is red
+//if they current time is before the time, then the textarea background is green
+  
 });
+//Add an icon to the save button
+
 
